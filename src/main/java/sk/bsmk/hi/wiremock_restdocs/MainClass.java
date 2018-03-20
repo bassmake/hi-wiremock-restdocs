80fd058dc1dd0fe673e6a0e7da9e8fd2a8213613
Miroslav Matejovsky <miroslav.matejovsky@wirecard.com>
2018-03-20 08:37:39 +0100

    Initial commit

---
+++ src/main/java/sk/bsmk/hi/wiremock_restdocs/MainClass.java
@@ -0,0 +1,13 @@
+package sk.bsmk.hi.wiremock_restdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClass {
+
+  public static void main(String ... args) {
+    SpringApplication.run(MainClass.class, args);
+  }
+
+}
